7be0e6080ba67aaeb1a31584b96cc049c3eff02a
Azizan <azizan_10@yahoo.com>
2016-01-25 01:01:04 +0800

    Calling functions and parameter using Javascript

---
+++ functions_parameter_javascript.js
@@ -0,0 +1,12 @@
+// Example
+function ourFunction(a, b) {
+  console.log(a - b);
+}
+ourFunction(10, 5); // Outputs 5
+
+// Only change code below this line.
+function myFunction(param1,param2){
+  console.log(param1 + param2);
+}
+myFunction(1,2);
+
