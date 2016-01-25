ce8bae7bbf6473f31046e57dc63a7c6a5ec2e675
Azizan Hasnor <azizan_10@yahoo.com>
2016-01-25 20:58:34 +0800

    Multiple usage of case in switch statement using Javascript

---
+++ case_multiple_javascript.js
@@ -0,0 +1,30 @@
+function myTest(val) {
+  var answer = "";
+  // Only change code below this line
+  switch(val) {
+    case answer = 1:
+    case answer = 2:
+    case answer = 3:
+      answer= "Low";
+      break;
+    case answer = 4:
+    case answer = 5:
+    case answer = 6:
+      answer= "Mid";
+      break;
+    case answer = 7:
+    case answer = 8:
+    case answer = 9:
+      answer= "High";
+      break;
+      
+  }
+  
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+myTest(8);
+
