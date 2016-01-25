b1a925452f377ffc55be1dd859fca85f0711164b
Azizan Hasnor <azizan_10@yahoo.com>
2016-01-25 21:12:38 +0800

    Combination of string and numbers inside CASE SWITCH using Javascript

---
+++ case_stringnumber_javascript.js
@@ -0,0 +1,35 @@
+function myTest(val) {
+  var answer = "";
+  // Only change code below this line
+  
+  switch (val) {
+    case answer = "bob":
+      answer = "Marley";
+      break;
+    case answer = 42:
+      answer = "The Answer";
+      break;
+    case answer = 1:
+      answer = "There is no #1";
+      break;
+    case answer = 99:
+      answer = "Missed me by this much!";
+      break;
+    case answer = 7:
+      answer = "Ate Nine";
+      break;
+    case answer = "John":
+      answer = "";
+      break;
+    case answer = 156:
+      answer = "";
+      break;
+  }
+  
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+myTest("bob");
+
