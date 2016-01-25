14db9831416f40f7def13e628dd2d7df6215526b
Azizan Hasnor <azizan_10@yahoo.com>
2016-01-25 18:40:55 +0800

    Using inequality to force boolean statement with Javascript

---
+++ inequality_operator_javascript.js
@@ -0,0 +1,11 @@
+// Setup
+function myTest(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+myTest(10);
+myTest(99);
