90ae447e2b2d1eaf7c274862a7995ec3be4b0571
Azizan Hasnor <azizan_10@yahoo.com>
2016-01-25 20:44:55 +0800

    Using switch case similar to if else with Javascript

---
+++ switch_case_javascript.js
@@ -0,0 +1,27 @@
+function myTest(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val){
+  case answer=1:
+     answer = "alpha";
+  break;
+  
+  case answer =2:
+  answer = "beta";
+  break;
+
+  case answer = 3:
+  answer = "gamma";
+  break;
+  
+  case answer = 4:
+  answer = "delta";
+  break;
+  }
+  // Only change code above this line  
+  return answer;  
+}
+
+// Change this value to test
+myTest(3);
+
