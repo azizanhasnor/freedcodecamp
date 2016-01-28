0d54fbc33c517a3762e6ef8b7d2bd5aef793bc96
Azizan Hasnor <azizan_10@yahoo.com>
2016-01-28 19:17:18 +0800

    Add array of object with JSON using Javascript

---
+++ object_JSON_javascript.js
@@ -0,0 +1,22 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1993,
+    "formats": [ 
+      "CS", 
+      "8T", 
+      "LP" ],
+    "gold": true
+  }, {
+    "artist": "Eric Clapton",
+    "title": "Tears In Heaven",
+    "release_year": 1990,
+    "formats": [
+      "Demo",
+      "CD",
+      "Vinyl"
+    ]
+  }
+];
+
